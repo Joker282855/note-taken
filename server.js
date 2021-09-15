3db7d6283362e623848609861d1075f816587959
unknown <jtjackson325@gmail.com>
2021-09-15 15:43:59 -0600

    pushing what I have to heroku

--- server.js
+++ server.js
@@ -1,7 +1,27 @@
+const { notes } = require("./db/db.json");
 const express = require('express');
 
 const app = express();
 
 app.listen(3001, () => {
-    console.log(`Serveer now runn on port 3001`);
+    console.log(`Server now runn on port 3001`);
+});
+
+function filterByQuery(query, notesArray) {
+    let filteredResults = notesArray;
+    if (query.title) {
+        filteredResults = filteredResults.filter(notes => notes.title === query.title);
+    }
+    if (query.text) {
+        filteredResults = filteredResults.filter(notes => notes.text === query.text);
+    }
+    return filteredResults;
+}
+
+app.get('/api/notes', (req, res) => {
+    let results = notes;
+    if (req.query) {
+        results = filterByQuery(req.query, results);
+    }
+    res.json(results);
 });
